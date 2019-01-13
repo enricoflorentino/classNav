8fe4d90486e8efe523c6f498d1938ece98d2f63f
ricoflorentino <enrico.f1998@gmail.com>
2019-01-12 20:53:38 -0500

    starting logic

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,12 @@ const { dialogflow } = require('actions-on-google');
 
 const app = dialogflow();
 
-app.intent('startRoute', conv => {
-    
-    conv.close('CLASSNAV');
+app.intent('startRoute', (conv, {number}) => {
+    if (number == 231) {
+        conv.close(`WHAT 231! You said ${number}`);
+    } else {
+        conv.close(`GO WHEREVER! You said ${number}`);
+    }
 });
 
 exports.mybackend = functions.https.onRequest(app);
