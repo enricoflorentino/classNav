ba225e94116b1e9ab50845fc0170b910fd0e660e
ricoflorentino <enrico.f1998@gmail.com>
2019-01-12 21:36:01 -0500

    2nd intent

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,24 @@ const { dialogflow } = require('actions-on-google');
 const app = dialogflow();
 
 app.intent('startRoute', (conv, {number}) => {
-    if (number == 231) {
-        conv.close(`WHAT 231! You said ${number}`);
-    } else {
-        conv.close(`GO WHEREVER! You said ${number}`);
-    }
+
+    // number arg is the desired room #
+   
+    if (number/1000 < 2) {
+        // first floor
+        if (number == 1012) {
+            conv.close(`Walk slightly to the right, room ${number} is up ahead.`);
+        } else {
+            conv.ask("Walk past the stairs that are ahead of you. Let me know when you're there.");    
+        }
+
+    } 
+     
 });
 
+
+app.intent('firstFloorRooms', (conv, {number}) => {
+    
+    conv.close(`Congrats`);
+});
 exports.mybackend = functions.https.onRequest(app);
