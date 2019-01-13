4add7ba40dada533632f04a18c9d3560b20f70ae
ricoflorentino <enrico.f1998@gmail.com>
2019-01-12 22:31:51 -0500

    global room #

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,14 @@ const { dialogflow } = require('actions-on-google');
 
 const app = dialogflow();
 
+// global variable to store desired room throughout destination
+global.temp = 1000;
+
 app.intent('startRoute', (conv, {number}) => {
 
     // number arg is the desired room #
-   
+
+    global.temp = number;
     if (number/1000 < 2) {
         // first floor
         if (number == 1012) {
@@ -14,14 +18,13 @@ app.intent('startRoute', (conv, {number}) => {
         } else {
             conv.ask("Walk past the stairs that are ahead of you. Let me know when you're there.");    
         }
-
+        
     } 
      
 });
 
 
-app.intent('firstFloorRooms', (conv, {number}) => {
-    
-    conv.close(`Congrats`);
+app.intent('firstFloorRooms', conv  => {
+    conv.close(`Congrats! ${global.temp}`);
 });
 exports.mybackend = functions.https.onRequest(app);
